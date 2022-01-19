e84e3f7bd9a369b8c6519c73fdaa90e06e7fec9b
eliblurr <elvissegbawu@gmail.com>
2022-01-19 15:12:36 +0000

    capture all react path

--- fastapi_boiler/main.py
+++ fastapi_boiler/main.py
@@ -8,10 +8,11 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="build/static"), name="static")
 templates = Jinja2Templates(directory="build")
 
-@app.get("/")
-async def serve_spa(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
-@app.get("/api")
+@app.get("/api") # use seperate application mount and proxy with nginx
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/{full_path:path}")
+async def serve_spa(request: Request, full_path: str):
+    print("full_path: "+full_path)
+    return templates.TemplateResponse("index.html", {"request": request})
